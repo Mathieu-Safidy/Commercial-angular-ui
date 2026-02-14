2c56d6f530602b43e25d7c5e34156b3392f97d53
Mathieu Safidy <safidy800@gmail.com>
2026-02-14 11:32:46 +0300

    Ajout de services utils

--- src/app/services/utils/utils.ts
+++ src/app/services/utils/utils.ts
@@ -6,17 +6,16 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root',
 })
 export class Utils {
-    private http = inject(HttpClient);
     
-    public Get = (url: string) => this.http.get(url);
-    public Post = (url: string, body: any) => this.http.post(url, body);
-    public Patch = (url: string, body: any) => this.http.patch(url, body);
-    public Delete = (url: string) => this.http.delete(url);
+    public static Get = (url: string) => inject(HttpClient).get(url);
+    public static Post = (url: string, body: any) => inject(HttpClient).post(url, body);
+    public static Patch = (url: string, body: any) => inject(HttpClient).patch(url, body);
+    public static Delete = (url: string) => inject(HttpClient).delete(url);
     
-    public PGet = (url: string) => firstValueFrom(this.http.get(url));
-    public PPost = (url: string, body: any) => firstValueFrom(this.http.post(url, body));
-    public PPatch = (url: string, body: any) => firstValueFrom(this.http.patch(url, body));
-    public PDelete = (url: string) => firstValueFrom(this.http.delete(url));
+    public static PGet = (url: string) => firstValueFrom(inject(HttpClient).get(url));
+    public static PPost = (url: string, body: any) => firstValueFrom(inject(HttpClient).post(url, body));
+    public static PPatch = (url: string, body: any) => firstValueFrom(inject(HttpClient).patch(url, body));
+    public static PDelete = (url: string) => firstValueFrom(inject(HttpClient).delete(url));
     
 
-}
+} 
